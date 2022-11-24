949ec92de621fcbc5af26dc2f44be9b9844b52a2
Bradford Toney <bradford.toney@gmail.com>
2022-11-24 16:28:50 -0500

    fix: remove dependencies

--- src/model.rs
+++ src/model.rs
@@ -1,29 +1,17 @@
 pub mod transaction {
-  use chrono::Utc;
-  use domain::id::TransactionId;
-  use sea_orm::entity::prelude::*;
+    use sea_orm::entity::prelude::*;
 
-  #[derive(Clone, Debug, PartialEq, Eq, DeriveEntityModel)]
-  #[sea_orm(table_name = "transaction")]
-  pub struct Model {
-    #[sea_orm(primary_key, auto_increment = false)]
-    pub id: TransactionId,
-    pub user_id: String,
-    pub created_at: DateTimeUtc,
-  }
-
-  impl ActiveModelBehavior for ActiveModel {}
-
-  #[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
-  pub enum Relation {}
-
-  impl Default for Model {
-    fn default() -> Self {
-      Self {
-        id: TransactionId::new(),
-        user_id: Default::default(),
-        created_at: Utc::now(),
-      }
+    #[derive(Clone, Debug, PartialEq, Eq, DeriveEntityModel)]
+    #[sea_orm(table_name = "transaction")]
+    pub struct Model {
+        #[sea_orm(primary_key, auto_increment = true)]
+        pub id: u64,
+        pub user_id: String,
+        pub created_at: DateTimeUtc,
     }
-  }
+
+    impl ActiveModelBehavior for ActiveModel {}
+
+    #[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
+    pub enum Relation {}
 }
--- src/version.rs
+++ src/version.rs
@@ -1,38 +1,40 @@
+use chrono::Utc;
 use sea_orm::{
-  ActiveModelBehavior, ActiveModelTrait, DatabaseConnection, EntityTrait, IntoActiveModel,
-  ModelTrait,
+    ActiveModelBehavior, ActiveModelTrait, ActiveValue::NotSet, DatabaseConnection, EntityTrait,
+    IntoActiveModel, ModelTrait, Set,
 };
 
 use crate::model;
 
 #[async_trait::async_trait]
 pub trait Version {
-  type VersionEntity;
+    type VersionEntity;
 
-  async fn create_version<Active>(&self, conn: &DatabaseConnection, user_id: String)
-  where
-    Self: ModelTrait + Clone,
-    // Entity needs have [`sea_orm::EntityTrait`]
-    Self::VersionEntity: EntityTrait,
-    // Entity Model needs to be [`From`] and [`sea_orm::ActiveModelTrait`]
-    // and [`sea_orm::ActiveModelBehavior`]
-    <Self::VersionEntity as EntityTrait>::Model: From<Self> + IntoActiveModel<Active>,
-    // We turn the Model into an ActiveModel which is denoted as `Active` here
-    Active: ActiveModelBehavior + ActiveModelTrait + Send + Sync,
-    // VersionEntity::Model -> ActiveModelTrait::Entity
-    // -> ActiveModelTrait::Entity::Model
-    //
-    // Needs to be [`sea_orm::IntoActiveModel`]
-    <<Active as sea_orm::ActiveModelTrait>::Entity as sea_orm::EntityTrait>::Model:
-      IntoActiveModel<Active>,
-  {
-    let txn = model::transaction::Model {
-      user_id,
-      ..Default::default()
-    };
-    txn.into_active_model().insert(conn).await.unwrap();
+    async fn create_version<Active>(&self, conn: &DatabaseConnection, user_id: String)
+    where
+        Self: ModelTrait + Clone,
+        // Entity needs have [`sea_orm::EntityTrait`]
+        Self::VersionEntity: EntityTrait,
+        // Entity Model needs to be [`From`] and [`sea_orm::ActiveModelTrait`]
+        // and [`sea_orm::ActiveModelBehavior`]
+        <Self::VersionEntity as EntityTrait>::Model: From<Self> + IntoActiveModel<Active>,
+        // We turn the Model into an ActiveModel which is denoted as `Active` here
+        Active: ActiveModelBehavior + ActiveModelTrait + Send + Sync,
+        // VersionEntity::Model -> ActiveModelTrait::Entity
+        // -> ActiveModelTrait::Entity::Model
+        //
+        // Needs to be [`sea_orm::IntoActiveModel`]
+        <<Active as sea_orm::ActiveModelTrait>::Entity as sea_orm::EntityTrait>::Model:
+            IntoActiveModel<Active>,
+    {
+        let txn = model::transaction::ActiveModel {
+            id: NotSet,
+            user_id: Set(user_id),
+            created_at: Set(Utc::now()),
+        };
+        txn.into_active_model().insert(conn).await.unwrap();
 
-    let versioned = <Self::VersionEntity as EntityTrait>::Model::from(self.clone());
-    versioned.into_active_model().insert(conn).await.unwrap();
-  }
+        let versioned = <Self::VersionEntity as EntityTrait>::Model::from(self.clone());
+        versioned.into_active_model().insert(conn).await.unwrap();
+    }
 }
